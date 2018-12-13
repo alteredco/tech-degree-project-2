6d98a769919fb46f1b01652cd1ec2b28af9b901c
Wendy <alteredco@gmail.com>
2018-12-13 13:36:57 +0100

    massive update

--- js/script.js
+++ js/script.js
@@ -6,62 +6,69 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// Global variables
 const page = document.querySelector('.page');
-const pageHeader = document.querySelector('.page-header cf');
-const studentList = document.querySelector('student-list');
+const pageHeader = document.querySelector('.page-header');
+const studentList = document.querySelector('.student-list').getElementsByTagName('li');
 
+let pageSize = 10;
+let pageNum = 1;
+let pageTotal = parseInt(studentList.length / pageSize);
 
-function search() {
+/***Search function with filter ***/
+function searchBar() {
    let searchBar = document.createElement('input');
-   searchBar.type = 'text';
-   searchBar.placeholder= 'Search…';
-})
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-function showPage(pageHeader) {
-   for(i=0;i<studentList.length;i++) {
-      let li = studentList[i];
-      if(li.childNodes[10]) {
-         li.style.display = 'none';
+   searchBar.setAttribute('type', 'text');
+   searchBar.setAttribute( 'placeholder', 'Search…');
+   searchBar.className = 'student-search';
+   pageHeader.appendChild(searchBar);
+   searchBar.addEventListener('keyup', (e) => {
+      let term = e.target.value
+      let searchLimit = 10;
+      for(i=0; i < studentList.length; i++) {
+         if(studentList[i].textContent.includes(term)) {
+            for(i=0; i< searchLimit; i++) {
+               console.log(studentList[i]);
+            }
+         } else {
+            studentList[i].style.display = 'none';
+         }
       }
+   })
+}
+searchBar();
+
+//*** ShowPage function to show selected page view of students***/
+function showPage(pageNum) {
+   // hide all students
+   for(i=0; i<studentList.length; i++) {
+      studentList[i].style.display = 'none';
    }
-}
+   // show list of students according to selected page number and size limit
+   for(i=0; i<pageSize; i++) {
+      studentIndex = pageNum * pageSize - pageSize + i;
+      studentList[studentIndex].style.display = 'block';
+   }
+};
+showPage(pageNum);
 
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-function appendPageLinks() {
-   let pageLink = document.createElement('link');
-   page.append('pageLink');
-}
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+/*** AppendPageLinks function to generate, append, and add  functionality to the pagination buttons.***/
+function appendPageLinks(pageTotal) {
+   let pageRange = document.createElement('ul');
+   pageRange.className = 'pagination';
+   page.appendChild(pageRange);
+   pageList =  Array.from(Array(pageTotal).keys());
+   for(i=0; i<pageList.length; i++) {
+      let pageBtn = document.createElement('li');
+      let pageLink = document.createElement('a');
+      pageLink.href = "#";
+      pageLink.textContent = i+1;
+      pageBtn.appendChild(pageLink);
+      pageRange.appendChild(pageBtn);
+      pageLink.addEventListener('click', (e) => {
+         pageNum = e.target.textContent;
+         showPage(pageNum);
+      })
+   }
+};
+appendPageLinks(pageTotal)
